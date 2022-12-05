81bb594c58141789358787921f6f919f581048a5
Dang Nguyen Anh Khoa <kdang@axon.com>
2022-12-05 09:37:04 +0700

    can use the left side

--- keyboards/handwired/dactyl_manuform/5x7/config.h
+++ keyboards/handwired/dactyl_manuform/5x7/config.h
@@ -34,3 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // WS2812 RGB LED strip input and number of LEDs
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 12
+#define SPLIT_HAND_PIN D1
+#define	SPLIT_HAND_PIN_LOW_IS_LEFT
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2000
