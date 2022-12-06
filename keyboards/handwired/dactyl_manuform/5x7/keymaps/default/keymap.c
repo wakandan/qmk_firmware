cbe5723388ae0d922e9a23e209744082f45938d0
Dang Nguyen Anh Khoa <kdang@axon.com>
2022-12-06 11:02:56 +0700

    work in progress

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
@@ -19,20 +19,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_LBRC,
-        KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   TAB_RO,
-        OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B, KC_B,
-        KC_CAPS,   KC_LGUI, TAB_L,   TAB_R,
-                                    TT(_FN), KC_SPC,
-                                    KC_END, KC_HOME,
-                                    KC_PSCR, TASK,
+        KC_LCTL,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_G,
+        KC_Z,      KC_Z,    KC_X,    KC_C,   KC_V,   KC_B, KC_B,
+                                     KC_CAPS, KC_A, KC_LALT, KC_C,
+                                     KC_D, KC_LWIN, KC_END, KC_HOME,
         // right hand
-                          KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_GRV,
-                          KC_RBRC, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
-                          KC_H,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                          KC_N,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_SLSH,
-                                            KC_LEFT, KC_UP,    KC_DOWN,  KC_RGHT,
+                                                KC_GRV, KC_EQL,KC_MINS,KC_0,KC_9,KC_8,KC_7,
+                                                  KC_BSLS,KC_P,KC_O,KC_I,KC_U,KC_Y,KC_RBRC,
+                                                  KC_QUOT,KC_SCLN,KC_L,KC_K,KC_J,KC_H,KC_H,
+                                             KC_SLSH,KC_SLSH,KC_DOT,KC_COMM,KC_M,KC_N,KC_N,
+                                            KC_LEFT, KC_UP,    KC_BSPC,  KC_RGHT,
              KC_BSPC, KC_ENT,
-             KC_PGUP, KC_PGDN,
-             KC_LCTL, KC_LALT
+             KC_UP, KC_SPC,
+             KC_LEFT, KC_DOWN
     )
 };
